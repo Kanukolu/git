8e9f5697e029abed7fe5af381df7c66f95185d9c
Kanukolu <104075953+Kanukolu@users.noreply.github.com>
2023-08-10 11:52:19 +0530

    adding new li

---
+++ 1.js
@@ -0,0 +1,15 @@
+
+  // var it=document.getElementsByClassName('list2-item');
+   
+   ///var it=document.getElementsByClassName('list2-item');
+   //it[0].textContent="new it"
+   //console.log(it);
+   //console.dir(document)
+   var it=document.getElementsByTagName('li');
+   it[4].textContent="new "
+   console.log(it);
+   console.dir(document)
+   it[4].style.backgroundColor="green"
+   it[4].style.fontWeight="bold"
+
+
