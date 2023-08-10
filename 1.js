61c5d6323d00063a0b16f0de4763b5b62de7a0b9
Kanukolu <104075953+Kanukolu@users.noreply.github.com>
2023-08-10 12:31:34 +0530

    updated js

--- 1.js
+++ 1.js
@@ -5,11 +5,22 @@
    //it[0].textContent="new it"
    //console.log(it);
    //console.dir(document)
-   var it=document.getElementsByTagName('li');
-   it[4].textContent="new "
-   console.log(it);
-   console.dir(document)
-   it[4].style.backgroundColor="green"
-   it[4].style.fontWeight="bold"
-
-
+   //var it=document.getElementsByTagName('li');
+    //it[4].textContent="new "
+    //console.log(it);
+    //console.dir(document)
+   // it[4].style.backgroundColor="green"
+    //it[4].style.fontWeight="bold"
+//queryselector
+var select=document.querySelector('.list-group-item:nth-child(2)')
+select.style.backgroundColor="green"
+var items=document.querySelector('.list-group-item:nth-child(3)')
+items.style.backgroundColor="black"
+//queryselectorall
+var item_2 =document.querySelectorAll("li:nth-child(odd)");
+for (let i = 0; i < item_2.length; i++) {
+    item_2[i].style.backgroundColor="green"
+    
+}
+var itemc_2 =document.querySelectorAll("li:nth-child(2)");
+itemc_2.style.color="green"
