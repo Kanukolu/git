56160a4ad16172787384e2c748871c1943cb6d6f
Kanukolu <104075953+Kanukolu@users.noreply.github.com>
2023-08-26 18:57:49 +0530

    response in browser

---
+++ response.js
@@ -0,0 +1,11 @@
+const http=require('http')
+const server=http.createServer((req,res)=>{
+    console.log(req.url,req.method,req.headers)
+    res.setHeader('Content-Type','text/html')
+    res.write("<html>")
+    res.write("<head><title>my</title></head>")
+    res.write("<body><h1>Hello from node js</h1></body>")
+    res.write("</html>")
+    res.end("hello")
+})
+server.listen(3300)
