f29eea79a68bb2b4ef5800a9f039a40312736feb
Kanukolu <104075953+Kanukolu@users.noreply.github.com>
2023-08-27 19:53:14 +0530

    express js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http=require('http')
-const routes=require('./routes')
 
-const server=http.createServer(routes)
+//console.log(routes.someText)
+const express=require("express")
+const app=express()
+app.use((req,res,next)=>{
+    console.log('in the middleware')
+    next()
+})
+
+app.use((req,res,next)=>{
+    console.log('in the 2middleware')
+    res.send('<h1>hello</h1>')
+})
+
+const server=http.createServer(app)
 
 server.listen(3310)
