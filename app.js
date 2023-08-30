48ec4a6b732cb493e7751d99907650549834a961
Kanukolu <104075953+Kanukolu@users.noreply.github.com>
2023-08-30 19:26:37 +0530

    filtering paths

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-const http=require('http')
+//const http=require('http')
 
 //console.log(routes.someText)
 const express=require("express")
 const bodyParser=require("body-parser")
 const app=express()
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require("./routes/shop")
 app.use(bodyParser.urlencoded({extended:false},{extends:false}))
+app.use(adminRoutes)
 // app.use("/",(req,res,next)=>{
 //     console.log('middleware')
 //     next()
 //})
-
-app.use("/add-product",(req,res,next)=>{
-    console.log('in 2middleware')
-    res.send('<form action="/product" method="POST"><input type="text" name="add product"><input type="text"><button type="submit">add product</button></input></input></form>')
-})
-app.use("/product",(req,res,next)=>{
-    console.log(req.body)
-    res.redirect("/")
-})
-app.use("/",(req,res,next)=>{
-    console.log('in')
-    res.send('<h1>hello</h1>')
+app.use(shopRoutes)
+app.use((req,res,next)=>{
+    res.status(404).send("<h1>page not found</h1>")
 })
 
-const server=http.createServer(app)
 
-server.listen(3310)
+
+app.listen(3310)
